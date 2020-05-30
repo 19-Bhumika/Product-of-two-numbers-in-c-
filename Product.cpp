b5a615a6ed732486b7a94f589b6e8192427789d9
19-Bhumika <64434384+19-Bhumika@users.noreply.github.com>
2020-05-31 00:50:53 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void Product()
+{
+	float a,b;
+	cout<<"To find product of two numbers:\n";
+	cout<<"\nEnter number 1:";
+	cin>>a;
+	cout<<"\nEnter number 2:";
+	cin>>b;
+	cout<<"\nProduct of two numbers:"<<(a*b);
+}
+int main()
+{
+	Product();
+	return 0;
+}
